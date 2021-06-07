80fa05133ccfcda188bc0532e6846978f9041f3d
aditi977 <aditid@vt.edu>
2021-06-07 14:44:22 -0400

    initial

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "ami_value" {
+  description = "ID of the VPC where to deploy in"
+  default     = "ami-0d5eff06f840b45e9"
+}
+
+variable "instance_type_value" {
+  description = "provide the instance type like t2.micro, t1.micro "
+  default     = "t2.micro"
+}
