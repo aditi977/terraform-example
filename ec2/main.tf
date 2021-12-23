ef164cb75991b0ba2e37560d7bde757516a33db9
Aditi Diwan <aditid@vt.edu>
2021-12-22 19:22:16 -0500

    redid ec2 instance

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,11 +1,12 @@
 provider "aws" {
-  profile    = "default"
   region     = "us-east-1"
+  profile = "default"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "ec2_example" {
   ami           = "${var.ami_value}"
   instance_type = "${var.instance_type_value}"
-  create_key_pair = false
-  key_name = "${var.key_name_value}"
+  tags = {
+    name = "My Terraform EC2"
+  }
 }
--- ec2/variables.tf
+++ ec2/variables.tf
@@ -1,13 +1,9 @@
 variable "ami_value" {
   description = "ID of the VPC where to deploy in"
-  default     = "ami-0d5eff06f840b45e9"
+  default     = "ami-0ed9277fb7eb570c9"
 }
 
 variable "instance_type_value" {
   description = "provide the instance type like t2.micro, t1.micro "
   default     = "t2.micro"
 }
-
-variable "key_name_value" {
-  description = "provide the key name"
- }
